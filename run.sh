84c8b9293545ef510abcc510a97c8dc0f5b8ab2c
CryptoSax <joshua@operatorfoundation.org>
2021-08-27 16:45:54 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 swift package update || exit 1
 swift build || exit 2
 
-sudo ./.build/x86_64-unknown-linux-gnu/debug/TunTesterCli en0
+sudo ./.build/x86_64-unknown-linux-gnu/debug/TunTesterCli --internet-interface en0
