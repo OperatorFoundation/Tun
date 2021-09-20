a1806f19d5dad3a0a790aa3a0fc430d2c4764081
CryptoSax <joshua@operatorfoundation.org>
2021-09-20 15:12:03 -0500

    updated Internet Protocols

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/OperatorFoundation/Datable.git", from: "3.0.6"),
-        .package(url: "https://github.com/OperatorFoundation/InternetProtocols.git", from: "1.1.4"),
+        .package(url: "https://github.com/OperatorFoundation/InternetProtocols.git", from: "2.0.0"),
         .package(url: "https://github.com/OperatorFoundation/TransmissionLinux.git", from: "0.3.4"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "0.4.4"),
         .package(url: "https://github.com/OperatorFoundation/Flower.git", from: "0.1.17"),
